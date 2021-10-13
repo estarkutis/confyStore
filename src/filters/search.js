88bff80b6c726c6b7262d3b1c3c9b4f7c5075348
kingeris <73307426+estarkutis@users.noreply.github.com>
2021-10-13 14:46:36 +0300

    Paieškos filtras
    nuo 334 iki 336 pamokos JS

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,32 @@
-import { getElement } from '../utils.js';
-import display from '../displayProducts.js';
-const setupSearch = () => {};
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
+const setupSearch = (store) => {
+  const form = getElement(".input-form");
+  const nameInput = getElement(".search-input");
+  form.addEventListener("keyup", function () {
+    const value = nameInput.value;
+    // console.log(value); konsolė su kuria search galime pamatyti kad veikia
+    if (value) {
+      const newStore = store.filter((product) => {
+        let { name } = product;
+        // console.log(name); galime pamatyti paieskos užklausos rexultatus
+        name = name.toLowerCase();
+        if (name.startsWith(value)) {
+          return product;
+        }
+      });
+      //   console.log(newStore); Sukuria masyvus paieskos lange
+      display(newStore, getElement(".products-container"));
+      if (newStore.length < 1) {
+        const products = getElement(".products-container");
+        products.innerHTML = `<h3 class="filter-error">
+        Sorry, no products matched your search
+        </h3>`;
+      }
+    } else {
+      display(store, getElement(".products-container"));
+    }
+  });
+};
 
 export default setupSearch;
