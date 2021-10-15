db2a39344e631a0b89420d4592ae1024a449dd90
kingeris <73307426+estarkutis@users.noreply.github.com>
2021-10-15 09:11:38 +0300

    Price filter
    338 pamoka keičiasi slankiojantis kainos matuoklis ir paveikslėliai keičiasi pagal kaina.

--- src/filters/price.js
+++ src/filters/price.js
@@ -14,6 +14,18 @@ const setupPrice = (store) => {
   priceInput.max = maxPrice;
   priceInput.min = 0;
   priceValue.textContent = `Value : $${maxPrice}`;
+
+  priceInput.addEventListener("input", function () {
+    const value = parseInt(priceInput.value);
+    // console.log(typeof value); galime matyti su parseInt skaičius consolėje
+    priceValue.textContent = `Value : $${value}`;
+    let newStore = store.filter((product) => product.price / 100 <= value);
+    display(newStore, getElement(".products-container"));
+    if (newStore.length < 1) {
+      const products = getElement(".products-container");
+      products.innerHTML = `<h3 class="filter-error">soory, no products matched your search</h3>`;
+    }
+  });
 };
 
 export default setupPrice;
